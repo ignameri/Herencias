db8bde456731231d0cf74a5811a69cebcca1ce1e
ignameritano <nachomeritano@gmail.com>
2023-03-29 18:38:10 -0300

    Cambios commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cliente cliente = new Cliente();
+
+        cliente.nombre = "Ignacio";
+        cliente.edad = 23;
+        cliente.telefono = 3546328;
+        cliente.credito = 25;
+
+        System.out.println(cliente.nombre);
+        System.out.println(cliente.edad);
+        System.out.println(cliente.telefono);
+        System.out.println(cliente.credito);
+
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.salario = 25000;
+
+        System.out.println(trabajador.salario);
     }
 }
+
+
+
+    class Persona {
+        int edad;
+        String nombre;
+        int telefono;
+     }
+
+    class Cliente extends Persona{
+        int credito;
+    }
+
+    class Trabajador extends Persona{
+        int salario;
+    }
+
+
+
+
+
+
+
+
+
